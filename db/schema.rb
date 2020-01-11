8e6d0296053cab5c90ed380c6b9ebb1d40484ecf
高波　由佳 <y-takanami@takanamiyukanoMacBook-Air.local>
2020-01-11 20:36:19 +0900

    [finish]4-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_074534) do
+ActiveRecord::Schema.define(version: 2020_01_11_111035) do
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
